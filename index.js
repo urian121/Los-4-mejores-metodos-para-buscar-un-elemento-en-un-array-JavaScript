69880431c3bfe10b92090010ccc1d6eae1dca4a2
urian <urian1213viera@gmail.com>
2023-02-25 20:07:54 -0500

    terminado

--- index.js
+++ index.js
@@ -1,72 +1,78 @@
 /**
- * Array.indexOf(): Este método devuelve la posición del primer elemento que 
+ * Array.indexOf(): Este método devuelve la posición del primer elemento que
  * coincide con el valor especificado en el array, 0 si se encuetra y -1 si no se encuentra.
  */
-let lenguajes         = ["PHP", "JavaScript", "NodeJS","Python"];
-let resultadoLenguaje = lenguajes.indexOf("JavaScript");
-console.log(resultadoLenguaje); 
-
-let contenedorRespuesta = document.querySelector('.contenedorRespuesta');
-contenedorRespuesta.innerHTML = resultadoLenguaje;
+let lenguajes = ["PHP", "JavaScript", "NodeJS", "Python"];
+let resultadoLenguaje = lenguajes.indexOf("Python");
+//console.log("Método indexOf() ", resultadoLenguaje);
 
 /**
  * Array.includes(): Este método devuelve true si el array incluye el elemento
  * especificado, y false en caso contrario.
  */
-let frutas = ["Manzana", "Banana", "Pera","Fresa"];
-let resultadoFrutas = frutas.includes("Pera");
-//console.log(resultadoFrutas); 
-
+let frutas = ["Manzana", "Banana", "Pera", "Fresa"];
+let resultadoFrutas = frutas.includes("Banana");
+//console.log("Método includes() ", resultadoFrutas);
 
 /**
  * Array.findIndex(): Este método devuelve la posición del primer elemento
- * que cumple con la función de prueba proporcionada.
+ * que cumple con la condicion, si no existe devuelve -1.
  */
-let numeros = [5, 10, 15, 20, 32];
-let resultadoNumero = numeros.findIndex(num => num > 10);
-//console.log(resultadoNumero); 
-
-
 let fruits = ["apple", "banana", "cherry"];
-let miResult = fruits.findIndex((fruit)=> {
-  return fruit === "cherry";
+let miResult = fruits.findIndex((fruit) => {
+  // console.log(fruit);
+  return fruit === "apple";
 });
-//console.log(miResult);
+//console.log("Método findIndex()", miResult);
 
 let miUsers = [
-    { name: "John", age: 30 },
-    { name: "Jane", age: 25 },
-    { name: "Jim", age: 35 }
-  ];
-  let resultadoUser = miUsers.findIndex(function(user) {
-    return user.name === "Jane";
-  });
-  //console.log(resultadoUser); 
-  
-
+  { name: "John", age: 30 },
+  { name: "Jane", age: 25 },
+  { name: "Jim", age: 35 },
+];
+let resultadoUser = miUsers.findIndex(function (user) {
+  //console.log(user);
+  return user.name === "John";
+});
+//PerfLogsconsole.log(resultadoUser);
 
 /**
  * Array.find(): Este método devuelve el primer elemento que
  * cumple con la función de prueba proporcionada.
  */
-let nombrePersonas = ["John","Jane","Jim","Emily","Michael","Sarah","David","Jessica","Robert","Emily"];
+let nombrePersonas = [
+  "John",
+  "Jane",
+  "Jim",
+  "Emily",
+  "Urian",
+  "Michael",
+  "Sarah",
+  "David",
+  "Jessica",
+  "Robert",
+  "Emily",
+];
 
-let resultado = nombrePersonas.find((persona) =>{
-    return persona === "Jim";
-}); 
-//console.log(resultado);
+let resultado = nombrePersonas.find((persona) => {
+  //console.log(persona);
+  return persona === "Urian";
+});
+//console.log("Método find()", resultado);
 
 /**
- * Array de objetos, encontrar un objeto en un array de objetos:
+ * Array de objetos, como encontrar un objeto en un array de objetos:
+ * Si existe devuelve el objeto de lo contrario undefined
  */
 
 let users = [
-    { name: "John", age: 30 },
-    { name: "Jane", age: 25 },
-    { name: "Jim", age: 35 }
-  ];
-  
-  let result = users.find((user)=> {
-    return user.name === "Jane";
-  });
-  //console.log(result); 
+  { name: "John", age: 30 },
+  { name: "Jane", age: 25 },
+  { name: "Jim", age: 35 },
+];
+
+let result = users.find((user) => {
+  //console.log(user);
+  return user.name === "Jim";
+});
+//console.log(result);
